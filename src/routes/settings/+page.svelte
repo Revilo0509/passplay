e94968e3274ed1cc2b45300ef6c0a4ce188068d2
Revilo0509 <oliver.elfverson@outlook.com>
2026-05-04 17:03:04 +0200

    Fix license

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { mode, setMode } from 'mode-watcher';
 	import * as Select from '$lib/components/ui/select';
+	import { MoveUpRight } from 'lucide-svelte';
 
 	let currentMode = $derived(mode.current ?? 'light');
 	let value = $derived(currentMode);
 </script>
 
-<div class="p-4">
+<div class="flex w-dvw flex-col justify-between p-4">
 	<div class="flex items-center gap-4">
 		<span class="text-lg">Theme:</span>
 		<Select.Root
@@ -22,5 +23,15 @@
 				<Select.Item value="dark">Dark</Select.Item>
 			</Select.Content>
 		</Select.Root>
+	</div>
+	<div class="flex justify-between text-sm opacity-75">
+		<div>
+			Copyright © <a href="https://revilo0509.net" class="underline">Revilo0509</a>
+			{new Date().getFullYear()}
+		</div>
+		<div class="flex items-center text-right">
+			<a href="https://github.com/Revilo0509/passplay" class="underline">Source Code</a>
+			<MoveUpRight class="ml-1 h-4 w-auto" />
+		</div>
 	</div>
 </div>
