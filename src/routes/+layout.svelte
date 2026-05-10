5034ab1e7d7692850f22966429d49794270d967d
Revilo0509 <oliver.elfverson@outlook.com>
2026-05-10 21:26:45 +0200

    Remove inlang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import type { Pathname } from '$app/types';
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import App from '$lib/components/App.svelte';
@@ -30,9 +26,3 @@
 
 	{@render children()}
 </App>
-
-<div style="display:none">
-	{#each locales as locale (locale)}
-		<a href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}>{locale}</a>
-	{/each}
-</div>
